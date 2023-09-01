d34407e1bf2a24382711d6b19fbe93b0fa0e3f0f
FATEC.SENAI <mbsoares.infor@gmail.com>
2023-08-31 21:59:48 -0300

    aula 6

--- aula6/funcoes.js
+++ aula6/funcoes.js
@@ -8,6 +8,41 @@ function escreverNaTela(){
     document.write("");
 }
 
+function somar(a, b){
+    return a + b;
+}
 function mostrarNome(nome){
     alert("meu nome é " + nome);
+}
+
+function exercicio6(){
+    
+    while(true){
+        var op = prompt("Digite a operação[+,-,sair]");
+        if(op == "sair"){
+            return;
+        }else if(op == "+"){
+            var v1 = prompt("digite o valor 1")
+            var v2 = prompt("digite o valor 2")
+            var soma = parseInt(v1) + parseInt(v2) ;
+            if(isNaN(soma)){
+                alert("VAlores digitados inválidos");
+            }else{
+                alert("a soma foi : " + soma);
+            }
+
+        }else if(op == "-"){
+            var v1 = prompt("digite o valor 1")
+            var v2 = prompt("digite o valor 2")
+            var subtracao = parseInt(v1) - parseInt(v2) ;
+            if(isNaN(subtracao)){
+                alert("VAlores digitados inválidos");
+            }else{
+                alert("a subtracao foi : " + subtracao);
+            }
+        }else{
+            alert("Opção inválida");
+        }
+        
+    }
 }
